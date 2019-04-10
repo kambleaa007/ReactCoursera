0ddbb7855b8b75ea96ef890a30521e743fd241b0
kambleaa007 <ashishkamble191191@gmail.com>
2019-04-10 17:42:16 +0530

    config reactstrap

--- one/confusion/src/App.js
+++ one/confusion/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import { Navbar, NavbarBrand } from 'reactstrap'; // bootstrap componenet imported from reactstrap
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">OneNavBar</NavbarBrand>
+        </div>
+      </Navbar>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
