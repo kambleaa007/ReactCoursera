a238bdc0b785bc057d7e7fbb865aa4ccf0f31f24
Ashish Kamble <ashishkamble191191@gmail.com>
2019-05-06 09:58:26 +0530

    Update dishes.js

--- four/src/shared/dishes.js
+++ four/src/shared/dishes.js
@@ -3,7 +3,7 @@ export const DISHES =
         {
         id: 0,
         name:'Uthappizza',
-        image: '/assets/images/uthappizza.png',
+        image: '/ReactCoursera/assets/images/uthappizza.png',
         category: 'mains',
         label:'Hot',
         price:'4.99',
@@ -13,7 +13,7 @@ export const DISHES =
         {
         id: 1,
         name:'Zucchipakoda',
-        image: '/assets/images/zucchipakoda.png',
+        image: '/ReactCoursera/assets/images/zucchipakoda.png',
         category: 'appetizer',
         label:'',
         price:'1.99',
@@ -23,7 +23,7 @@ export const DISHES =
         {
         id: 2,
         name:'Vadonut',
-        image: '/assets/images/vadonut.png',
+        image: '/ReactCoursera/assets/images/vadonut.png',
         category: 'appetizer',
         label:'New',
         price:'1.99',
@@ -33,7 +33,7 @@ export const DISHES =
         {
         id: 3,
         name:'ElaiCheese Cake',
-        image: '/assets/images/elaicheesecake.png',
+        image: '/ReactCoursera/assets/images/elaicheesecake.png',
         category: 'dessert',
         label:'',
         price:'2.99',
